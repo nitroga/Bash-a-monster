0269fbd8c10941bbbc32eccffbcf23b4d7ad3258
Alexander Sundvisson <nitrogaplays@gmail.com>
2022-08-30 12:35:46 +0200

    first commit

---
+++ Bash-a-monster/Player.cs
@@ -0,0 +1,7 @@
+public class Player
+{
+    public string name = "Player";
+    public int hp = 100;
+    public int attack = 15;
+    public int defence = 5;
+}
---
+++ Bash-a-monster/Slime.cs
@@ -0,0 +1,7 @@
+public class Slime
+{
+    public string name = "Slime";
+    public int hp = 50;
+    public int attack = 10;
+    public int defence = 5;
+}
